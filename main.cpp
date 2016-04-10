914c556d0698cfa44a0c921b2aafa2bc542142c7
kesireddyadarsh <akesireddy@nevada.unr.edu>
2016-04-10 08:16:13 -0700

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 #include <cmath> 
 #include <cstdlib>
 #include <cassert>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -33,7 +35,7 @@ public:
     double z_outputVal;
     void setOutputVal(double val) { z_outputVal = val; }
     double getOutputVal(void) const { return z_outputVal; }
-    void feedForward(const Layer &prevLayer);
+    void feedForward(const Layer prevLayer);
     double transferFunction(double x);
 };
 
@@ -48,13 +50,19 @@ Neuron::Neuron(unsigned numOutputs, unsigned myIndex)
     z_myIndex = myIndex;
 }
 
-void Neuron::feedForward(const Layer &prevLayer)
+void Neuron::feedForward(const Layer prevLayer)
 {
     double sum = 0.0;
+    bool debug_sum_flag = false;
     
     for (unsigned n = 0; n < prevLayer.size(); ++n) {
-        sum += prevLayer[n].getOutputVal() *
-        prevLayer[n].z_outputWeights[z_myIndex].weight;
+        if(debug_sum_flag == true){
+            cout<<prevLayer[n].getOutputVal()<<endl;
+            cout<<&prevLayer[n].z_outputWeights[z_myIndex];
+            cout<<prevLayer[n].z_outputWeights[z_myIndex].weight;
+        }
+        sum += prevLayer[n].getOutputVal() * prevLayer[n].z_outputWeights[z_myIndex].weight;
+        //cout<<"This is sum value"<<sum<<endl;
     }
     
     z_outputVal = Neuron::transferFunction(sum);
@@ -92,19 +100,18 @@ Net::Net(vector<unsigned> &topology){
         
         if(numOutputs>5){
             cout<<"Stop it number outputs coming out"<<numOutputs<<endl;
-            exit(0);
+            exit(10);
         }
         z_layer.push_back(Layer());
         for(int numNeurons = 0; numNeurons <= topology[numLayers]; numNeurons++){
-            cout<<"This is neuron number:"<<numNeurons<<endl;
+            //cout<<"This is neuron number:"<<numNeurons<<endl;
             z_layer.back().push_back(Neuron(numOutputs, numNeurons));
         }
     }
-    
 }
 
 void Net::feedForward(const vector<double> &inputVals){
-    assert(inputVals.size() == z_layer[0].size()-1);
+    //assert(inputVals.size() == z_layer[0].size()-1);
     
     for (unsigned i=0; i<inputVals.size(); ++i) {
         z_layer[0][i].setOutputVal(inputVals[i]);
@@ -133,6 +140,7 @@ double Net::backProp(const vector<double> &targetVals){
     return z_error;
 }
 
+//Error showed be class (use construct)
 
 //This is for population of neural network
 
@@ -143,14 +151,18 @@ public:
     vector<Net> popVector;
     void runNetwork(vector<double> &inputVal, vector<double> &targetVal,int numNN);
     vector<double> error;
+    vector<vector<double>> overallError;
+    vector<double> newerror;
     void sortError();
+    void mutation(int numNN);
+    void newpopulation(vector<double> error);
 };
 
 // variables used: indiNet -- object to Net
 population::population(int numNN,vector<unsigned> &topology){
     
     for (int populationNum = 0 ; populationNum<numNN; populationNum++) {
-        cout<<"This is neural network:"<<populationNum<<endl;
+        //cout<<"This is neural network:"<<populationNum<<endl;
         Net indiNet(topology);  
         popVector.push_back(indiNet);
     }
@@ -160,33 +172,76 @@ population::population(int numNN,vector<unsigned> &topology){
 void population::runNetwork(vector<double> &inputVal, vector<double> &targetVal,int numNN){
     for (int temp=0 ; temp< numNN; temp++) {
         //Run neural network.
-        popVector[temp].feedForward(inputVal);
+         popVector[temp].feedForward(inputVal);
         double temp_1 = popVector[temp].backProp(targetVal);
         error.push_back(temp_1);
     }
     cout<<"This is size of error"<<error.size()<<endl;
     sortError();
+    bool error_check_flag = true;
+    if(error_check_flag==true){
+        for (int temp =0 ; temp<error.size(); temp++) {
+            cout<<error[temp]<<endl;
+        }
+    }
+    overallError.push_back(error);
+    newpopulation(error);
+    error.clear();
+    mutation(numNN);
+    cout<<"This is total erro:"<<overallError.size()<<endl;
 }
 
 void population::sortError(){
     sort(error.begin(), error.end());
-    for(int temp =0; temp<=error.size();temp++){
+}
+
+void population::newpopulation(vector<double> error){
+    int size = error.size()/4;
+    for (int temp =0 ; temp< size; temp++) {
+        newerror.push_back(error[temp]);
+        error.erase(error.begin());
+    }
+    cout<< "This is error size now::"<<error.size()<<endl;
+    for (int temp =0 ; temp<error.size(); temp++) {
         cout<<error[temp]<<endl;
+    }
+    int looprotate = error.size()/2;
+    for (int temp = 0; temp<looprotate; temp++ ) {
+        int temp_1 = error.size();
+        int number_1 = (rand() % temp_1)+1;
+        int number_2 = (rand() % temp_1)+1;
+        if(error[number_1]<error[number_2]){
+            newerror.push_back(error[number_1]);
+            error.erase(error.begin()+(number_1-1));
+        }else if (error[number_1]>error[number_2]){
+            newerror.push_back(error[number_2]);
+            error.erase(error.begin()+(number_2-1));
+        }else{
+            
+        }
+    }
+    cout<<"This is new error vector::"<<endl;
+    for (int temp =0 ; temp<newerror.size(); temp++) {
+        cout<<newerror[temp]<<endl;
     }
 }
 
+void population::mutation(int numNN){
+    
+}
 
 //This is main function
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, World!\n";
+    //cout << "Hello, World!\n";
+    srand(time(NULL));
     vector<double> inputVal;
     vector<double> outputVal;
     vector<double> resultVal;
     vector<double> targetVal;
     
-    int numNN=100;
+    int numNN=20;
     vector<unsigned> topology;
     topology.clear();
     topology.push_back(2);
@@ -194,11 +249,51 @@ int main(int argc, const char * argv[]) {
     topology.push_back(1);
     population mypop(numNN,topology);
     
-    inputVal.push_back(1.0);
-    inputVal.push_back(1.0);
-    targetVal.push_back(1.0);
-    mypop.runNetwork(inputVal, targetVal, numNN);
+    bool z_debugger_flag = false;
     
+    if(z_debugger_flag == true){
+    for (int i=0; i<100; i++) {
+        int number = (rand() % 4)+1;
+        switch (number) {
+            case 1:
+                inputVal.push_back(1.0);
+                inputVal.push_back(1.0);
+                targetVal.push_back(1.0);
+                break;
+            
+            case 2:
+                inputVal.push_back(1.0);
+                inputVal.push_back(0.0);
+                targetVal.push_back(1.0);
+                break;
+            
+            case 3:
+                inputVal.push_back(0.0);
+                inputVal.push_back(1.0);
+                targetVal.push_back(1.0);
+                break;
+            
+            case 4:
+                inputVal.push_back(0.0);
+                inputVal.push_back(0.0);
+                targetVal.push_back(0.0);
+                break;
+            
+            default:
+                inputVal.push_back(0.0);
+                inputVal.push_back(0.0);
+                targetVal.push_back(0.0);
+                break;
+        }
+        
+        mypop.runNetwork(inputVal, targetVal, numNN);
+    }
+    }else{
+        inputVal.push_back(1.0);
+        inputVal.push_back(0.0);
+        targetVal.push_back(1.0);
+        mypop.runNetwork(inputVal, targetVal, numNN);
+    }
     
     //
     //for (int i=0; i<=numNN; i++) {
